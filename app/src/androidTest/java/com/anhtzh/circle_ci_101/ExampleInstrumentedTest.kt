7c6c029de37989caa3ba5073f9eeb7b02b0073a7
Anh Tran <anh.tran@quoine.com>
2018-08-11 15:18:49 +0700

    Init android project

---
+++ app/src/androidTest/java/com/anhtzh/circle_ci_101/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.anhtzh.circle_ci_101
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.anhtzh.circle_ci_setup_with_andoid_app_101", appContext.packageName)
+    }
+}
